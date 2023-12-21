a54b4c3529f2f27c7139e601dc20630eec323338
bakumi1 <monosenz@bk.ru>
2023-12-21 08:31:31 +0300

    UPDATE V1 21.12.2023 ALPHA

--- js/script.js
+++ js/script.js
@@ -1,24 +1,104 @@
-function toggleMenu() {
-    var menuButton = document.querySelector('.menu-button');
-    var navigation = document.querySelector('nav ul');
-
-    menuButton.classList.toggle('open');
-    navigation.classList.toggle('open');
+function toggleLanguageMenu() {
+    var languageMenu = document.querySelector('.language-menu');
+    languageMenu.classList.toggle('open');
 }
 
-// ... предыдущий код ...
+function changeLanguage(language) {
+    var languageButton = document.querySelector('.language-button');
+    var selectedLanguageItem = document.querySelector('.language-menu-item.selected');
+    if (selectedLanguageItem) {
+        selectedLanguageItem.classList.remove('selected');
+    }
+
+    var elementsRU = document.querySelectorAll('.ru');
+    var elementsEN = document.querySelectorAll('.en');
+
+    elementsRU.forEach(function (el) {
+        el.style.display = (language === 'RU') ? 'inline' : 'none';
+    });
+
+    elementsEN.forEach(function (el) {
+        el.style.display = (language === 'EN') ? 'inline' : 'none';
+    });
+
+    if (language === 'RU') {
+        languageButton.textContent = 'RU';
+    } else if (language === 'EN') {
+        languageButton.textContent = 'EN';
+    }
+
+    var newSelectedLanguageItem = document.querySelector('.language-menu-item.' + language);
+    newSelectedLanguageItem.classList.add('selected');
+
+    // Закрыть меню после смены языка
+    toggleLanguageMenu(); 
+}
+
+function fadeOut() {
+    document.body.classList.add('fade-out');
+    setTimeout(function () {
+        window.location.href = "services.html"; // Измените на нужный URL
+    }, 500); // Время анимации в миллисекундах (здесь 500 мс)
+}
+
+
+function toggleMenu() {
+    var menu = document.querySelector('.side-menu');
+    var menuButton = document.querySelector('.menu-button');
+
+    menu.classList.toggle('open');
+    menuButton.classList.toggle('open');
+}
+
+document.addEventListener('click', function (event) {
+    var languageMenu = document.querySelector('.language-menu');
+    var languageButton = document.querySelector('.language-button');
+
+    if (!languageMenu.contains(event.target) && !languageButton.contains(event.target)) {
+        languageMenu.classList.remove('open');
+    }
+});
+
+function closeMenu() {
+    var sideMenu = document.querySelector('.side-menu');
+    sideMenu.classList.remove('open');
+}
+
+
+function toggleMenu() {
+    var menu = document.querySelector('.side-menu');
+    var menuButton = document.querySelector('.menu-button');
+
+    menu.classList.toggle('open');
+    menuButton.classList.toggle('open');
+}
+
+function closeMenu() {
+    var sideMenu = document.querySelector('.side-menu');
+    sideMenu.classList.remove('open');
+}
 
 function toggleLanguageMenu() {
     var languageMenu = document.querySelector('.language-menu');
     languageMenu.classList.toggle('open');
 }
 
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
     var languageMenu = document.querySelector('.language-menu');
     var languageButton = document.querySelector('.language-button');
+    var menu = document.querySelector('.side-menu');
+    var menuButton = document.querySelector('.menu-button');
+    var closeButton = document.querySelector('.close-button');
 
     if (!languageMenu.contains(event.target) && !languageButton.contains(event.target)) {
         languageMenu.classList.remove('open');
+    }
+
+    if (!menu.contains(event.target) && !menuButton.contains(event.target)) {
+        menu.classList.remove('open');
+        menuButton.classList.remove('open');
+        closeButton.classList.remove('open');
+        closeMenu();
     }
 });
 
@@ -49,7 +129,7 @@ function changeLanguage(language) {
     var newSelectedLanguageItem = document.querySelector('.language-menu-item.' + language);
     newSelectedLanguageItem.classList.add('selected');
 
-    toggleLanguageMenu(); 
+    toggleLanguageMenu();
 }
 
 function fadeOut() {
@@ -60,28 +140,8 @@ function fadeOut() {
 }
 
 
-function toggleMenu() {
-    var menu = document.querySelector('.side-menu');
-    var menuButton = document.querySelector('.menu-button');
 
-    menu.classList.toggle('open');
-    menuButton.classList.toggle('open');
-}
 
-document.addEventListener('click', function (event) {
-    var menu = document.querySelector('.side-menu');
-    var menuButton = document.querySelector('.menu-button');
-    var closeButton = document.querySelector('.close-button');
 
-    if (!menu.contains(event.target) && !menuButton.contains(event.target)) {
-        menu.classList.remove('open');
-        menuButton.classList.remove('open');
-        closeButton.classList.remove('open');
-        closeMenu();
-    }
-});
 
-function closeMenu() {
-    var sideMenu = document.querySelector('.side-menu');
-    sideMenu.classList.remove('open');
-}
+
